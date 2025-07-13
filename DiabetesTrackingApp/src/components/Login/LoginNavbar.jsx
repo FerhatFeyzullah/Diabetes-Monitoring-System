71697f664826dba5dcd64b5a55cdc3c5b12e3712
FerhatFeyzullah <ferhatfeyzullah@hotmail.com>
2025-07-13 11:16:58 +0300

    screenshots added and web icon edited

--- DiabetesTrackingApp/src/components/Login/LoginNavbar.jsx
+++ DiabetesTrackingApp/src/components/Login/LoginNavbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function LoginNavbar() {
   return (
     <div className="login-navbar">
-      <div className="login-navbar-title">Diyabet Takip Uygulaması</div>
+      <div className="login-navbar-title">Diyabet Kontrol</div>
       <div className="login-navbar-title-alt">
         Diyabeti Takip Et, Sağlığını Kontrol Et!
       </div>
